5b3fbabfd0f5ffa4b77d64e8c0bb21456e239287
Burk0 <Burk0@users.noreply.github.com>
2014-12-05 21:50:15 +0100

    Update x^n.c

--- x^n.c
+++ x^n.c
@@ -1,52 +1,45 @@
-// uloha-8-2.c -- Tyzden 8 - Uloha 2
-// David Buransky, 17.11.2014 19:51:08
-
+//pow my fnc
 #include <stdio.h>
-#define num 2.0
+#include <string.h>
 
 // vypocita x^n
 double xn(double x, int n)
 {
-    double exp=1.0,exp2=0.0,k=x*x,pole[1000];
-    int i,j,pom=1;
-    exp2=exp;
+
+    int exp=n,zvysok,k;
+  double pom=x,vysledok=1.0;
+    int i,pole[1000000];
     if(n==0)
         return 1;
     if(n==1)
         return x;
     else
+    {
+        while(exp!=0)
         {
-            //pole[0]=exp;
-            for(i=0; i<n-1; i++)
-                {
-                    //printf("%d\n",i);
-                    exp*=x;
-                    pole[i]=exp;
-                    //printf("%lf\n",pole[i]);
-                    if(exp>n)
-                        {
-                            pom=n-exp2;
-                            for(j=2; j<=i; j++)
-                                {
-                                    k*=k;
-
-                                }
-                      //      printf("%lf\n",k);
-                            break;
-                            pole[i]='\0';
-                        }
-                        //printf("%lf\n",pole[5]);
-                    exp2=exp;
-                }
-                    k*=pole[pom];
-
-
-                    //for(i=0;i<strlen(pole);i++)
-                        //printf("%lf\n",pole[i]);
-
+            zvysok=exp%2;
+            pole[i]=zvysok;
+            exp/=2;
+          //printf("%d\n",pole[i]);
+            i++;
         }
-       // printf("%lf\n",k);
-    return k;
+        pole[i]='\0';
+        k=i;
+        i=0;
+        while(i!=k)
+        {
+          	//printf ("Prebehol som\n");
+            if(pole[i]==1)
+            {
+              vysledok*=pom;
+              //printf("Vysledok %d\n",vysledok);
+            }
+
+       	pom*=pom;
+        i++;
+        }
+        return vysledok;
+    }
 }
 
 // ukazkovy test
@@ -54,6 +47,6 @@ int main(void)
 {
     int i;
     for (i = 0; i < 32; i++)
-        printf("2^%d = %lf\n", i, xn(2.0, i));
+        printf("2^%d = %lf\n", i, xn(2.0,i ));
     return 0;
 }
